8fb727d5ce5ee8aab15767fb16f805b40fd0d828
Garrett Gorman <garrettgorman1@gmail.com>
2023-10-16 15:34:52 -0400

    added taskee and category routes

--- server/api/index.js
+++ server/api/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/category", require("./category"));
-router.use("/skills", require("./skills"));
-router.use("/task", require("./task"));
+// router.use("/skills", require("./skills"));
+// router.use("/task", require("./task"));
 router.use("/taskee", require("./taskee"));
-router.use("/tasker", require("./tasker"));
+// router.use("/tasker", require("./tasker"));
 module.exports = router;
