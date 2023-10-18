01aa39a14069295af9e07b07672db47a5373e10a
calvieTech <cthang94@calvietech.com>
2023-10-18 11:42:38 -0400

    added auth page & some server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 // );
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(
+	express.urlencoded({ limit: "50mb", extended: true })
+);
 
 app.use("/auth", require("./auth"));
 
