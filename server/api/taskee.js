bb365d40931a285e79bec0b4d8576f0642ff9f31
calvieTech <cthang94@calvietech.com>
2023-10-20 16:06:21 -0400

    wired the reducer for auth

--- server/api/taskee.js
+++ server/api/taskee.js
@@ -4,45 +4,46 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 router.get("/", async (req, res, next) => {
-  try {
-    const allTaskees = await prisma.taskee.findMany({
-      include: {
-        Skills: true,
-      },
-    });
-    res.send(allTaskees);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const allTaskees = await prisma.taskee.findMany({
+			include: {
+				Skills: true,
+			},
+		});
+		res.send(allTaskees);
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.get("/:id", async (req, res, next) => {
-  try {
-    const singleTaskee = await prisma.taskee.findUnique({
-      where: {
-        id: +req.params.id,
-      },
-      include: {
-        Skills: true,
-      },
-    });
-    res.send(singleTaskee);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const singleTaskee = await prisma.taskee.findUnique({
+			where: {
+				id: +req.params.id,
+			},
+			include: {
+				Skills: true,
+			},
+		});
+		res.send(singleTaskee);
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.get("/reviews/:id", async (req, res, next) => {
-  try {
-    const singleTaskeeReviews = await prisma.taskeeReview.findMany({
-      where: {
-        taskeeId: +req.params.id,
-      },
-    });
-    res.send(singleTaskeeReviews);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const singleTaskeeReviews =
+			await prisma.taskeeReview.findMany({
+				where: {
+					taskeeId: +req.params.id,
+				},
+			});
+		res.send(singleTaskeeReviews);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
