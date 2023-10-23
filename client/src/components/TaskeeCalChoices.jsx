60665c40f2fc9c5af5abc8f03404094f883aa110
Kevin Cook <140981683+Kevin-T-Cook@users.noreply.github.com>
2023-10-23 13:43:35 -0700

    changes to Calendar 10.23

--- client/src/components/TaskeeCalChoices.jsx
+++ client/src/components/TaskeeCalChoices.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { useDispatch } from "react-redux";
 import { postTaskeeScheduleThunk } from "../store/taskee";
+
 
 export default function TaskeeCalChoices({ selectedDay, showErrorMessage }) {
     const dispatch = useDispatch();
--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -6,6 +6,12 @@ const REGISTER_TASKER = "REGISTER_TASKER";
 const LOGIN_TASKER = "LOGIN_TASKER";
 const LOGOUT_TASKER = "LOGOUT_TASKER";
 
+const REGISTER_TASKEE = "REGISTER_TASKEE";
+const LOGIN_TASKEE = "LOGIN_TASKEE";
+const LOGOUT_TASKEE = "LOGOUT_TASKEE";
+
+// TASKER
+
 const registerTasker = (tasker) => ({
 	type: REGISTER_TASKER,
 	payload: tasker,
@@ -19,6 +25,23 @@ const loginTasker = (tasker) => ({
 const logoutTasker = (tasker) => ({
 	type: LOGOUT_TASKER,
 	payload: tasker,
+});
+
+// TASKEE
+
+const registerTaskee = (taskee) => ({
+	type: REGISTER_TASKEE,
+	payload: taskee,
+});
+
+const loginTaskee = (taskee) => ({
+	type: LOGIN_TASKEE,
+	payload: taskee,
+});
+
+const logoutTaskee = (taskee) => ({
+	type: LOGOUT_TASKEE,
+	payload: taskee,
 });
 
 export const loginTaskerThunk =
@@ -65,6 +88,50 @@ export const registerTaskerThunk =
 		}
 	};
 
+	export const loginTaskeeThunk =
+	(credentials) => async (dispatch) => {
+		try {
+			const { email, password } = credentials;
+			const res = await axios
+				.post(`${BASE_URL}/auth/auth_taskee/login`, {
+					email,
+					password,
+				})
+				.catch((err) => {
+					console.log("error: ", err);
+					return;
+				});
+
+			console.log(`res: `, res);
+			return dispatch(loginTaskee(res));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+export const registerTaskeeThunk =
+	(credentials) => async (dispatch) => {
+		try {
+			console.log(`credentials: `, credentials);
+			const { fName, lName, email, phone, password } =
+				credentials;
+			const { data: taskee } = await axios.post(
+				`${BASE_URL}/auth/auth_taskee/register`,
+				{
+					fName,
+					lName,
+					email,
+					phone,
+					password,
+				}
+			);
+			console.log(taskee);
+			return dispatch(registerTaskee(taskee));
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 const initialState = {
 	user: {},
 };
@@ -76,6 +143,12 @@ export default function (state = initialState, action) {
 		case LOGIN_TASKER:
 			return { ...state, user: action.payload };
 		case LOGOUT_TASKER:
+			return { ...state, user: null };
+		case REGISTER_TASKEE:
+			return { ...state, user: action.payload };
+		case LOGIN_TASKEE:
+			return { ...state, user: action.payload };
+		case LOGOUT_TASKEE:
 			return { ...state, user: null };
 		default:
 			return state;
