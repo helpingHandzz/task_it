f2f05003feec284360dbda8778ef27f9cc2624f7
Garrett Gorman <garrettgorman1@gmail.com>
2023-10-24 14:40:33 -0400

    create task page and routes

--- client/src/pages/CreateTask.jsx
+++ client/src/pages/CreateTask.jsx
@@ -1,33 +1,107 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { postTaskThunk } from "../store/task";
 
-function CreateTask() {
+function CreateTask({ sub }) {
+  let { state } = useLocation();
+  console.log("subcat", state.subcat);
+  const dispatch = useDispatch();
+  //STARTING
+  const [startStreet, setStartStreet] = useState("");
+  const [startCity, setStartCity] = useState("");
+  const [startState, setStartState] = useState("");
+  const [startZip, setStartZip] = useState("");
+  const [startSuite, setStartSuite] = useState("");
+  //ENDING
+  const [endStreet, setEndStreet] = useState("");
+  const [endCity, setEndCity] = useState("");
+  const [endState, setEndState] = useState("");
+  const [endZip, setEndZip] = useState("");
+  const [endSuite, setEndSuite] = useState("");
+
+  //OTHER INFORMATION
+  const [vehicleRequired, setVehicleRequired] = useState(false);
+  const [estTimeCommit, setEstTimeCommit] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleCreateTask = (e) => {
+    e.preventDefault();
+    dispatch(
+      postTaskThunk({
+        taskerId: 1,
+        subcategoryId: state.subcat.id,
+        description: description,
+        isCompleted: false,
+        vehicleRequired: vehicleRequired,
+        isAssigned: false,
+        estTimeCommitment: estTimeCommit,
+        startingStreet: startStreet,
+        startingCity: startCity,
+        startingState: startState,
+        startingZip: startZip,
+        startingSuite: startSuite,
+        endingStreet: endStreet,
+        endingCity: endCity,
+        endingState: endState,
+        endingZip: endZip,
+        endingSuite: endSuite,
+      })
+    );
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={(e) => handleCreateTask(e)}>
         {/* STARTING ADDRESS */}
         <h2>Starting Address</h2>
         <label>Street</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setStartStreet(e.target.value)} />
         <label>City</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setStartCity(e.target.value)} />
         <label>State</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setStartState(e.target.value)} />
         <label>Zip Code</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setStartZip(e.target.value)} />
         <label>Apt / Suite</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setStartSuite(e.target.value)} />
         {/* ENDING ADDRESS */}
         <h2>Ending Address</h2>
         <label>Street</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setEndStreet(e.target.value)} />
         <label>City</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setEndCity(e.target.value)} />
         <label>State</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setEndState(e.target.value)} />
         <label>Zip Code</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setEndZip(e.target.value)} />
         <label>Apt / Suite</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setEndSuite(e.target.value)} />
+        {/* Vehicle and Time */}
+        <h2>Other Information</h2>
+        <p>Vehicle Required?</p>
+        <input
+          type="radio"
+          id="yes"
+          name="vehicleRequired"
+          value={true}
+          onChange={() => setVehicleRequired(true)}
+        />
+        <label htmlFor="yes">Yes</label>
+        <input
+          type="radio"
+          id="no"
+          name="vehicleRequired"
+          value={false}
+          onChange={() => setVehicleRequired(false)}
+        />
+        <label htmlFor="no">No</label>
+        <br />
+        <label>Estimated Time Commitment</label>
+        <input type="text" onChange={(e) => setEstTimeCommit(e.target.value)} />
+        <label>Description</label>
+        <input type="text" onChange={(e) => setDescription(e.target.value)} />
+        <button type="submit">Continue</button>
       </form>
     </div>
   );
--- client/src/store/task.js
+++ client/src/store/task.js
@@ -4,6 +4,9 @@ const BASE_URL = "http://localhost:8080";
 
 const GET_TASKS = "GET_TASKS";
 const GET_TASK = "GET_TASK";
+const POST_TASK = "POST_TASK";
+const EDIT_TASK = "EDIT_TASK";
+const DELETE_TASK = "DELETE_TASK";
 
 const getTasks = (tasks) => ({
   type: GET_TASKS,
@@ -15,6 +18,22 @@ const getTask = (task) => ({
   payload: task,
 });
 
+const postTask = (task) => ({
+  type: POST_TASK,
+  payload: task,
+});
+
+const editTask = (task) => ({
+  type: EDIT_TASK,
+  payload: task,
+});
+
+const deleteTask = (task) => ({
+  type: DELETE_TASK,
+  payload: task,
+});
+
+//ALL TASKS
 export const getTasksThunk = () => async (dispatch) => {
   try {
     const { data: tasks } = await axios.get(`${BASE_URL}/api/task`);
@@ -25,11 +44,49 @@ export const getTasksThunk = () => async (dispatch) => {
   }
 };
 
+//SINGLE TASK
 export const getTaskThunk = (id) => async (dispatch) => {
   try {
     const { data: task } = await axios.get(`${BASE_URL}/api/task/${id}`);
     console.log("data", task);
     return dispatch(getTask(task));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//POST TASK
+export const postTaskThunk = (data) => async (dispatch) => {
+  try {
+    const { data: task } = await axios.post(`${BASE_URL}/api/task/new`, {
+      ...data,
+    });
+    return dispatch(postTask(task));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//EDIT TASK
+export const editTaskThunk = (data) => async (dispatch) => {
+  try {
+    const { data: task } = await axios.put(
+      `${BASE_URL}/api/task/edit/${data.id}`,
+      data
+    );
+    return dispatch(editTask(task));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//DELETE TASK
+export const deleteTaskThunk = (id) => async (dispatch) => {
+  try {
+    const { data: task } = await axios.delete(
+      `${BASE_URL}/api/task/delete/${id}`
+    );
+    return dispatch(deleteTask(task));
   } catch (error) {
     console.error(error);
   }
@@ -46,6 +103,22 @@ export default function (state = initialState, action) {
       return { ...state, allTasks: action.payload };
     case GET_TASK:
       return { ...state, singleTask: action.payload };
+    case POST_TASK:
+      state.allTasks.push(action.payload);
+      return state;
+    case EDIT_TASK:
+      return {
+        allTasks: state.allTasks.map((task) =>
+          task.id === action.payload.id ? action.payload : task
+        ),
+      };
+    case DELETE_TASK:
+      return {
+        ...state,
+        allTasks: state.allTasks.filter(
+          (task) => task.id !== action.payload.id
+        ),
+      };
     default:
       return state;
   }
