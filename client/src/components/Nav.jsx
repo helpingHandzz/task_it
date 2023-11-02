5e8b1a7c39bd52e7bfda6b58a85fe8cfbbfdd1ee
Kevin Cook <140981683+Kevin-T-Cook@users.noreply.github.com>
2023-11-02 12:31:21 -0700

    Reworked Nav and App to Render Based On User Role

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,13 +3,10 @@ import logo from "../assets/taskit-high-resolution-logo-color-on-transparent-bac
 import { useDispatch, useSelector } from "react-redux";
 import { logoutTaskerThunk } from "../store/auth";
 
-function Nav() {
-  // grab token from localStorage
-  const token = useSelector((state) => state.auth?.user?.token);
+function Nav( { isAuthenticated, isTasker, isTaskee } ) {
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  console.log(`token: `, token);
 
   // handler for log out
   const logout = async (e) => {
@@ -19,10 +16,107 @@ function Nav() {
   };
 
 
-  // if token is present, render Navbar with Logout
-  if (token) {
+  // if token is present && isTasker is true, render Navbar with Logout
+  if (isAuthenticated) {
+    if (isTasker) {
+      return (
+        <nav className="h-14 bg-white w-full flex justify-between fixed">
+          <div className="flex flex-col justify-center ml-2">
+            <img className="h-10" src={logo} alt="logo" />
+          </div>
+          <div className="flex align-middle">
+            <Link
+              className="flex flex-col justify-center ml-5 text-xl font-bold hover:text-cyan-700"
+              to={"/"}
+            >
+              Home
+            </Link>
+            <section className="flex flex-col justify-center ml-5">
+              {"|"}
+            </section>
+            <Link
+              className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
+              to={"/categories"}
+            >
+              Categories
+            </Link>
+            <section className="flex flex-col justify-center mr-5">
+              {"|"}
+            </section>
+            <Link
+              className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
+              to={"/tasks"}
+            >
+              Tasks
+            </Link>
+            <section className="flex flex-col justify-center mr-5">
+              {"|"}
+            </section>
+            <Link
+              className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
+              to={"/account"}
+            >
+              Account
+            </Link>
+            <section className="flex flex-col justify-center mr-5">
+              {"|"}
+            </section>
+            <button
+              className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </div>
+        </nav>
+      );
+    } else if (isTaskee) {
+      return (
+        <nav className="h-14 bg-white w-full flex justify-between fixed">
+          <div className="flex flex-col justify-center ml-2">
+            <img className="h-10" src={logo} alt="logo" />
+          </div>
+          <div className="flex align-middle">
+            <Link
+              className="flex flex-col justify-center ml-5 text-xl font-bold hover:text-cyan-700"
+              to={"/"}
+            >
+              Home
+            </Link>
+            <section className="flex flex-col justify-center ml-5">
+              {"|"}
+            </section>
+            <Link
+              className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
+              to={"/tasks"}
+            >
+              Tasks
+            </Link>
+            <section className="flex flex-col justify-center mr-5">
+              {"|"}
+            </section>
+            <Link
+              className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
+              to={"/account"}
+            >
+              Account
+            </Link>
+            <section className="flex flex-col justify-center mr-5">
+              {"|"}
+            </section>
+            <button
+              className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </div>
+        </nav>
+      );
+    }
+  } else {
     return (
-      <nav className="h-14 bg-white w-full flex justify-between fixed">
+      <nav className="h-14 bg-amber-100 w-full flex justify-between fixed">
         <div className="flex flex-col justify-center ml-2">
           <img className="h-10" src={logo} alt="logo" />
         </div>
@@ -43,52 +137,20 @@ function Nav() {
           <section className="flex flex-col justify-center mr-5">{"|"}</section>
           <Link
             className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
-            onClick={logout}
+            to={"/auth_tasker"}
           >
-            Logout
+            Login/Sign Up
+          </Link>
+          <section className="flex flex-col justify-center mr-5">{"|"}</section>
+          <Link
+            className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
+            to={"/auth_taskee"}
+          >
+            Become a Taskee
           </Link>
         </div>
       </nav>
     );
-  }
-
-
-  return (
-    <nav className="h-14 bg-amber-100 w-full flex justify-between fixed">
-      <div className="flex flex-col justify-center ml-2">
-        <img className="h-10" src={logo} alt="logo" />
-      </div>
-      <div className="flex align-middle">
-        <Link
-          className="flex flex-col justify-center ml-5 text-xl font-bold hover:text-cyan-700"
-          to={"/"}
-        >
-          Home
-        </Link>
-        <section className="flex flex-col justify-center ml-5">{"|"}</section>
-        <Link
-          className="flex flex-col justify-center mx-3 text-xl font-bold  hover:text-cyan-700"
-          to={"/categories"}
-        >
-          Categories
-        </Link>
-        <section className="flex flex-col justify-center mr-5">{"|"}</section>
-        <Link
-          className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
-          to={"/auth_tasker"}
-        >
-          Login
-        </Link>
-        <section className="flex flex-col justify-center mr-5">{"|"}</section>
-        <Link
-          className="flex flex-col justify-center mr-3 text-xl font-bold  hover:text-cyan-700"
-          to={"/auth_taskee"}
-        >
-          Become a Taskee
-        </Link>
-      </div>
-    </nav>
-  );
+  };
 }
-
 export default Nav;
