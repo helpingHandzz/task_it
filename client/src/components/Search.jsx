beed88e75b7d6796100c996490a366246b475103
calvieTech <cthang94@calvietech.com>
2023-11-13 22:53:43 -0500

    finished search feature, need more styling

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,38 +1,76 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Search({ categories }) {
 	const [searchValue, setSearchValue] = useState("");
+	const [filteredData, setFilteredData] = useState([]);
 
 	const handleSearchValueChange = (event) => {
-		// event.preventDefault();
-		console.log(`categories: `, categories);
-		// console.log(event.target.value);
-		// setSearchValue(event.target.value);
+		event.preventDefault();
+		setSearchValue(event.target.value);
+		const newFilter = categories.filter((category) => {
+			return category.categoryName
+				.toLowerCase()
+				.includes(searchValue.toLowerCase());
+		});
+
+		if (searchValue === "") {
+			setFilteredData([]);
+		} else {
+			setFilteredData(newFilter);
+		}
 	};
 
-	// if (searchValue.length > 0) {
-	// 	console.log(`categories: `, categories);
-	// 	categories.filter((category) => {
-	// 		return category.categoryName.match(searchValue);
-	// 	});
-	// }
-
+	const location = "http://" + window.location.host;
 	return (
-		<div className="flex flex-col justify-center items-center gap-4">
-			<h2 className="text-4xl text-center">
+		<div className="flex flex-col justify-center items-center gap-0">
+			<h2 className="text-3xl text-center my-4">
 				What can we help you with?
 			</h2>
-			<div className="inline-flex justify-center">
-				{/* <input
+			<div className="inline-flex justify-center ">
+				<input
 					type="search"
-					className="flex m-4 px-6 w-96 border border-black"
+					className="placeholder:italic placeholder:text-slate-40 flex mx-1 my-0 px-4 w-96 border border-black rounded py-2 text-lg focus:outline-none"
 					name=""
 					id=""
 					value={searchValue}
 					onChange={handleSearchValueChange}
 					placeholder="Find the right help..."
-				/> */}
+				/>
 			</div>
+			{filteredData.length != 0 && (
+				<div
+					className="dataResult"
+					style={{
+						width: "438px",
+						paddingLeft: "20px",
+						paddingRight: "20px",
+						marginLeft: "5px",
+						marginRight: "5px",
+						height: "200px",
+						color: "black",
+						backgroundColor: "white",
+						boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+						overflow: "hidden",
+						overflowY: "auto",
+						WebkitScrollSnapType: "none",
+						marginTop: "0px",
+					}}>
+					{filteredData.slice(0, 5).map((val, key) => {
+						console.log(`val of filteredData: `, val);
+						return (
+							<Link
+								key={val.id}
+								className="dataItem w-full h-5 flex items-center justify-start py-4 hover:bg-yellow-300 hover:cursor-pointer"
+								to={`${location}/categories/${val.id}`}>
+								<p className="mx-3 block">
+									{val.categoryName}
+								</p>
+							</Link>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 }
